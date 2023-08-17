fe6d570d507927d457d6d0c0adb0dab53cdd39a2
Ved Jha <vedprakash@fanclash.in>
2023-08-17 08:12:34 +0530

    euclidean mtd2

--- mathematicalprograms/gcd.py
+++ mathematicalprograms/gcd.py
@@ -1,11 +1,21 @@
 # naive solution -- T.C. -- O(min(a,b))
-def find_gcd(a:int,b:int):
-    while (a!=b):
-        if (a>b):
-            a -= b
-        else:
-            b -= a
-    return a
+
+# Euclidean mtd 1
+# def find_gcd(a:int,b:int):
+#     while (a!=b):
+#         if (a>b):
+#             a -= b
+#         else:
+#             b -= a
+#     return a
 
 
-print(find_gcd(12,15))
+# print(find_gcd(12,15))
+
+# Euclidean mtd 2 
+def find_gcd(a:int, b:int):
+    if b==0:
+        return a
+    return find_gcd(b, a%b)
+
+print(find_gcd(12,151))
