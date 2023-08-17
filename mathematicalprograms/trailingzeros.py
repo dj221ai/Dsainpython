b4ab130fe93d37deec591375a04400e607a132fa
Ved Jha <vedprakash@fanclash.in>
2023-08-17 07:21:57 +0530

    naive and optimise solution

--- mathematicalprograms/trailingzeros.py
+++ mathematicalprograms/trailingzeros.py
@@ -1,9 +1,33 @@
+# naive solution -- Time complexity is O(n)
+def count_zeros(n):
+    fact=1
+    i=1
+    if n==0:
+        return 1
+    while (i<=n):
+        fact=fact*i
+        i+=1
+    
+    print("facotiral is ", fact)
+
+    count=0
+    while(fact%10==0):
+        count+=1
+        fact=fact//10
+        
+
+    print("zeros are: ", count)
+
+count_zeros(10)
+
+
+# optimized solution 
 def countZeros(n):
     i=5
-    sum1=0
+    result=0
     while (i<=n):
-        sum1=n//i + sum1
+        result=n//i + result
         i*=5
-    return sum1
+    return result
 
 print(countZeros(1000))
