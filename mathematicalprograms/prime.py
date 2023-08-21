c6ba0392f3a8e20424d9b88c29920212e827cc32
Ved Jha <vedprakash@fanclash.in>
2023-08-21 11:03:54 +0530

    added efficient mtd 1

--- mathematicalprograms/prime.py
+++ mathematicalprograms/prime.py
@@ -1,5 +1,5 @@
 # naive approach
-def isPrime(n:int):
+def isPrimeNaive(n:int):
     return False if n==1 else all(n%i != 0 for i in range(2, n))
 
     # if n==1:
@@ -11,4 +11,22 @@ def isPrime(n:int):
 
 if __name__=='__main__':
     n=int(input("Enter nos: "))
-    print(isPrime(n))
+    print(isPrimeNaive(n))
+
+
+# Efficient Mtd1
+
+def isPrimeMtdOne(n:int):
+    if n==1:
+        return False
+    i=2
+    while (i**2 <= n):
+        if n%i==0:
+            return False
+        i+=1
+    return True
+
+
+if __name__=='__main__':
+    n=int(input("Enter nos: "))
+    print(isPrimeMtdOne(n))
