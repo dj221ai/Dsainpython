87f4241debad53c4ef03a84ba0ba8cba4d7f8fb4
Ved Jha <vedprakash@fanclash.in>
2023-08-27 18:32:53 +0530

    recursive optimised computing

--- mathematicalprograms/computing.py
+++ mathematicalprograms/computing.py
@@ -13,3 +13,24 @@ def findPowerValues(n,x):
 if __name__=='__main__':
     n,x=map(int, input("Enter values: ").split())
     findPowerValues(n,x)
+
+
+
+def power(x,n):
+    # if n%2==0:
+    #     return pow(x,n//2)*pow(x, n//2)
+    # else:
+    #     return pow(x, n-1)*x
+
+    if n==0:
+        return 1
+    temp = power(x, n//2)
+    temp*=temp
+    return temp if n%2==0 else temp*x
+
+
+
+
+if __name__=='__main__':
+    x,n=map(int, input("Enter values: ").split())
+    print(power(x, n))
