8aa4c615e75e0219aebd26fbe2e55a803634d2e3
Ved Jha <vedprakash@fanclash.in>
2023-09-02 19:34:54 +0530

    remove dups

--- lists/removeduplicates.py
+++ lists/removeduplicates.py
@@ -13,3 +13,22 @@ def removeDuplicates(lst):
 if __name__=='__main__':
     lst = [10,20,20,30,30,30,30,40,5]
     print(removeDuplicates(lst))
+
+# print("without using new list")
+
+# without using new list returning new size
+
+def removeWithoutNewListNaive(lst):
+    res=1
+    for i in range(1, len(lst)):
+        if lst[res-1] != lst[i]:
+            lst[res]=lst[i]
+            res+=1
+
+    return res
+
+
+
+if __name__=='__main__':
+    lst = [10,20,30,30,30,30,80,80]
+    print(removeWithoutNewListNaive(lst))
