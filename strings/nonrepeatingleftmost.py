5125dd443143ff0c40e4912f3e839d336255b8c8
Ved Jha <vedprakash@fanclash.in>
2023-11-01 22:13:49 +0530

    non-repeating-leftmost

--- strings/nonrepeatingleftmost.py
+++ strings/nonrepeatingleftmost.py
@@ -1,15 +1,38 @@
+# def leftmostNonRepeating(s):
+#     visited=[0]*256
+
+#     for i in range(len(s)):
+#         visited[ord(s[i])]+=1
+
+#     for j in range(len(s)):
+#         if visited[ord(s[j])] == 1:
+#             return j
+#     else:
+#         return -1
+    
+
+# efficient 1
+import sys
+CHAR=256
 def leftmostNonRepeating(s):
-    visited=[0]*256
-
+    fIndex=[-1]*CHAR
     for i in range(len(s)):
-        visited[ord(s[i])]+=1
+        if fIndex[ord(s[i])]==-1:
+            fIndex[ord(s[i])]=i
+        else:
+            fIndex[ord(s[i])]=-2
 
-    for j in range(len(s)):
-        if visited[ord(s[j])] == 1:
-            return j
-    else:
+
+    res=sys.maxsize
+    for i in range(CHAR):
+        if fIndex[i] >=0:
+            res=min(res, fIndex[i])
+
+    if res==sys.maxsize:
         return -1
+    else:
+        return res
 
 
 
-print(leftmostNonRepeating("abcabcd"))
+print(leftmostNonRepeating("geeksforgeeks"))
